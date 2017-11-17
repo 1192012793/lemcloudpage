0823dbc070b243260ea3f61d061056d7e93393ff
lyx <584719733@qq.com>
2017-11-18 00:10:24 +0800

    添加webpack的sass依赖

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import Layout from './templates/default/Layout.vue'
+require("./static/default/style/common/common.scss")
 //import RoleList from './templates/default/authority/role/List.vue'
 
 Vue.use(ElementUI)
