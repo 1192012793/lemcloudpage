51feded0a7203b9612e1082951522098475e9253
lyx <584719733@qq.com>
2017-11-20 08:45:11 +0800

    头部

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import Layout from './templates/default/Layout.vue'
-require("./static/default/style/common/common.scss")
+require("./static/default/style/common/common.css")
 require("./static/default/style/fonts/iconfont.css")
 import 'element-ui/lib/theme-chalk/display.css';
 //import RoleList from './templates/default/authority/role/List.vue'
